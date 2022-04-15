f1f96f265d1e32fa422feb7d284eb8dc9f54e7a6
shobhit jaryal <jaryalshaurya2203@gmail.com>
2022-04-15 12:37:51 +0530

    about updated

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -7,12 +7,18 @@ function AboutPage() {
       <div className='about'>
         <h1>About this Project</h1>
         <p>
-          This Feedback UI project was made using React with React-Router-DOM.
+          This Feedback UI project was made using React and React-Router-DOM.
+          Hooks such as useState , useEffect and useContext(for state-management) are used. 
+          Data is send to JSON Server(backend)
           Pages are dynamicaly loaded.
         </p>
         <span>
           <strong>React:</strong> <sup>^</sup>18.0.0 <br />
-          <strong>React-router-dom:</strong> <sup>^</sup>6.3.0
+          <strong>React-router-dom:</strong> <sup>^</sup>6.3.0<br />
+          <strong>Heroku:</strong> <sup>^</sup>17.60.0<br />
+          <strong>JSON Server:</strong> <sup>^</sup>0.17.0<br />
+          <strong>Netlify</strong><br />
+          <li>created by <a href="https://www.linkedin.com/in/shobhit-jaryal/">Shobhit Jaryal</a></li>
         </span>
         <br />
         <p>
